00c8b00a2381743a1db40567ee942ddc8f8de9c7
Levi Salazar <lsalazar-2026039@kinal.edu.gt>
2026-06-11 12:10:22 -0600

    feat(categoria) se creo modelo de Categoria #16

---
+++ src/org/Deco/model/Categoria.java
@@ -0,0 +1,36 @@
+package org.Deco.model;
+
+public class Categoria {
+    private int idCategoria; 
+    private String nombreCategoria; 
+
+    public Categoria() {}
+
+    public Categoria(String nombreCategoria) {
+        this.nombreCategoria = nombreCategoria;
+    }
+ 
+    public Categoria(int idCategoria, String nombreCategoria) {
+        this.idCategoria = idCategoria;
+        this.nombreCategoria = nombreCategoria;
+    }
+    
+ 
+
+    public int getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(int idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getNombreCategoria() {
+        return nombreCategoria;
+    }
+
+    public void setNombreCategoria(String nombreCategoria) {
+        this.nombreCategoria = nombreCategoria;
+    }
+   
+}
