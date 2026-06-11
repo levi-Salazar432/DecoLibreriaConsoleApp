17dd3510ecd486d48b46de22cf1f2e22dc26da93
Levi Salazar <lsalazar-2026039@kinal.edu.gt>
2026-06-11 10:53:20 -0600

    000 primer commit - se agrego la estructura del proyecto

---
+++ src/org/Deco/systen/Main.java
@@ -0,0 +1,13 @@
+package org.Deco.systen;
+
+/**
+ *
+ * @author Levi Salazar
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
+    
+}
