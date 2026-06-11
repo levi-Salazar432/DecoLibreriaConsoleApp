aa5dcdc76c2dbab5333025b5357b6f9084599767
Diego Raguay <draguay-2026067@kinal.edu.gt>
2026-06-11 12:12:40 -0600

    feat(cliente) se creo Modelo de Cliente y se guerdo en netbeans #006.1

--- src/org/Deco/model/Cliente.java
+++ src/org/Deco/model/Cliente.java
@@ -1,13 +1,50 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package org.Deco.model;
 
-/**
- *
- * @author informatica
- */
 public class Cliente {
-    
+    private Long cui;
+    private String nombreCliente;
+    private String apellidoCliente;
+    private String correoElectronico;
+
+    public Cliente() {
+    }
+
+    public Cliente(Long cui, String nombreCliente, String apellidoCliente, String correoElectronico) {
+        this.cui = cui;
+        this.nombreCliente = nombreCliente;
+        this.apellidoCliente = apellidoCliente;
+        this.correoElectronico = correoElectronico;
+    }
+
+    public Long getCui() {
+        return cui;
+    }
+
+    public void setCui(Long cui) {
+        this.cui = cui;
+    }
+
+    public String getNombreCliente() {
+        return nombreCliente;
+    }
+
+    public void setNombreCliente(String nombreCliente) {
+        this.nombreCliente = nombreCliente;
+    }
+
+    public String getApellidoCliente() {
+        return apellidoCliente;
+    }
+
+    public void setApellidoCliente(String apellidoCliente) {
+        this.apellidoCliente = apellidoCliente;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
 }
