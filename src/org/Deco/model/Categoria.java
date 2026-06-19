31278294b2601fe19cfaa1fd53eca782332d1b67
Levi Salazar <lsalazar-2026039@kinal.edu.gt>
2026-06-19 11:02:53 -0600

    feat(categoria) se agrego la clase POJO para categoria #16

--- src/org/Deco/model/Categoria.java
+++ src/org/Deco/model/Categoria.java
@@ -1,28 +1,36 @@
 package org.Deco.model;
 
 public class Categoria {
-    private int idCategoria; 
-    private String nombreCategoria; 
-
-    public Categoria() {}
-
-    public Categoria(String nombreCategoria) {
-        this.nombreCategoria = nombreCategoria;
-    }
- 
-    public Categoria(int idCategoria, String nombreCategoria) {
+    /*
+    create table categorias(
+	id_categoria int primary key auto_increment,
+    nombre_categoria varchar(100)
+);
+    */    
+    //atributos de clase 
+    private int idCategoria ;
+    private String nombreCategoria;
+    
+    //construcctores 
+    //vacio
+public Categoria() {
+          
+}
+//lleno o con parametros 
+ public Categoria(int idCategoria, String nombreCategoria) {
         this.idCategoria = idCategoria;
         this.nombreCategoria = nombreCategoria;
     }
-    
- 
+ //personalizado 
+      
+    //getter and setter 
 
     public int getIdCategoria() {
-        return idCategoria;
+      return idCategoria;
     }
 
     public void setIdCategoria(int idCategoria) {
-        this.idCategoria = idCategoria;
+         this.idCategoria = idCategoria;
     }
 
     public String getNombreCategoria() {
@@ -30,7 +38,10 @@ public class Categoria {
     }
 
     public void setNombreCategoria(String nombreCategoria) {
+        //Formatear a Mayuscula 
+        String nombreCategoriaMayuscula = nombreCategoria.toUpperCase();
+        //Formatear a Iniciar con Mayuscula
         this.nombreCategoria = nombreCategoria;
     }
-   
+ 
 }
