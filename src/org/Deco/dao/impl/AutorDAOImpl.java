994a48eae568a123d878f4f9fbca71d045268740
Kenneth Palencia <kpalencia-2023259@kinal.edu.gt>
2026-06-19 11:56:10 -0600

    feat(Autor) Se agrego la implementacion para el DAO de Autor, sin configuracion #13

---
+++ src/org/Deco/dao/impl/AutorDAOImpl.java
@@ -0,0 +1,43 @@
+package org.Deco.dao.impl;
+import org.Deco.util.Conexion;
+import org.Deco.model.Autor;
+import org.Deco.dao.AutorDAO;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.sql.PreparedStatement;
+import java.sql.Connection;
+import java.sql.CallableStatement;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+
+public class AutorDAOImpl implements AutorDAO{
+
+    @Override
+    public boolean insertar(Autor autor) {
+        return false;
+    }
+
+    @Override
+    public List<Autor> listar() {
+        return null;
+    }
+
+    @Override
+    public Autor buscar(int id_autor) {
+        return null;
+    }
+
+    @Override
+    public boolean actualizar(Autor autor) {
+        return false;
+    }
+
+    @Override
+    public boolean eliminar(int id_autor) {
+        return false;
+    }
+    
+    
+    
+}
