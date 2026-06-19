68522e870533e3541d25b31166d6f82ca1a12c13
Diego Raguay <draguay-2026067@kinal.edu.gt>
2026-06-19 11:36:14 -0600

    feat(cliente) se agrego la interfaz DAO para cliente ClienteDAO #7

---
+++ src/org/Deco/dao/ClienteDAO.java
@@ -0,0 +1,14 @@
+package org.Deco.dao;
+
+import java.util.List; 
+import org.Deco.model.Cliente; 
+
+public interface ClienteDAO {
+    
+    boolean insertar(Cliente cliente);
+    List<Cliente> Listar(); 
+    Cliente buscar(long cui);
+    boolean actualizar(Cliente cliente); 
+    boolean eliminar(long cui); 
+    
+}
