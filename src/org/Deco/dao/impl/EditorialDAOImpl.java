86e65723172d6cd10bc1c958dddeb57e3b291f9a
Joshua Suy <jsuy-2026803@kinal.edu.gt>
2026-06-19 11:56:20 -0600

    feat(editorial) se agrego la implementacion para el DAO de editorial sin configuracion #23

---
+++ src/org/Deco/dao/impl/EditorialDAOImpl.java
@@ -0,0 +1,36 @@
+package org.Deco.dao.impl;
+
+import org.Deco.model.Editorial;
+import org.Deco.dao.EditorialDAO;
+
+import java.util.List;
+
+
+public class EditorialDAOImpl implements EditorialDAO{
+
+    @Override
+    public boolean insertar(Editorial editorial) {
+        return false;
+    }
+
+    @Override
+    public List<Editorial> listar() {
+        return null;
+    }
+
+    @Override
+    public Editorial buscar(String nit) {
+        return null;
+    }
+
+    @Override
+    public boolean actualizar(Editorial editorial){
+        return false;
+    }
+
+    @Override
+    public boolean eliminar(String nit) {
+        return false;
+    }
+    
+}
