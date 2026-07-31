218397ced2006e71170c5e28a8a0445a09285176
Diego Raguay <draguay-2026067@kinal.edu.gt>
2026-07-31 07:20:56 -0600

    feat(Main) se cargo la escena de inicio de sesion en el Main agregado a eso se corrigio el usuario y contraseña de MySQL

--- src/org/Deco/systen/Main.java
+++ src/org/Deco/systen/Main.java
@@ -1,9 +1,26 @@
 package org.Deco.systen;
 
-public class Main {
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
 
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+       launch(args); 
+    }
+
+    @Override
+    public void start(Stage escenarioPrincipal) throws Exception {
+        Parent raiz = FXMLLoader.load(getClass().getResource("/org/Deco/view/InicioSesionView.fxml")); 
+        
+        Scene escena = new Scene(raiz); 
+        
+        escenarioPrincipal.setScene(escena); 
+        escenarioPrincipal.show(); 
     }
     
 }
