f99383eab7b583bdaa490daaf8a7e92ee83d817c
Esteban2023297 <emaldonado-2023297@kinal.edu.gt>
2026-08-14 11:45:55 -0600

    Se finalizo lo que es el AutoresLibroDAO

--- src/org/Deco/dao/AutoresLibroDAO.java
+++ src/org/Deco/dao/AutoresLibroDAO.java
@@ -1,7 +1,21 @@
 package org.Deco.dao;
 
+import java.util.List;
+import org.Deco.model.AutoresLibro;
 
 public interface AutoresLibroDAO {
 
+    boolean insertar(AutoresLibro autoresLibro);
 
+    List<AutoresLibro> listarTodos();
+
+    AutoresLibro buscar(int idAutorLibro);
+
+    boolean actualizar(AutoresLibro autoresLibro);
+
+    boolean eliminar(int idAutorLibro);
+
+    AutoresLibro buscarPorId(int id);
+
+    boolean crear(AutoresLibro autoresLibro);
 }
