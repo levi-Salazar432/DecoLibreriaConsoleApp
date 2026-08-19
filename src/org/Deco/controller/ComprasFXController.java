5ca421843130baa0899f4e9010a6d81172f470ce
Levi Salazar <lsalazar-2026039@kinal.edu.gt>
2026-08-19 16:04:06 -0600

    feat(compras) Se creo la clase Modelo, Controller, View y Style y se agrego estructura a modelo compras

---
+++ src/org/Deco/controller/ComprasFXController.java
@@ -0,0 +1,14 @@
+package org.Deco.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class ComprasFXController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+      
+    }    
+    
+}
---
+++ src/org/Deco/model/Compras.java
@@ -0,0 +1,62 @@
+package org.Deco.model;
+
+import java.time.LocalDateTime;
+
+public class Compras {
+//    -- compras
+//create table compras(
+//	no_compra int primary key auto_increment,
+//    fecha_compra timestamp default current_timestamp,
+//    total_compra decimal(8,2),
+//    cui_cliente bigint
+
+    private int noCompra; 
+    private LocalDateTime fechaCompra;  
+    private float totalCompra; 
+    private int cui; 
+
+    public Compras() {
+    }
+
+    public Compras(int noCompra, LocalDateTime fechaCompra, float totalCompra, int cui) {
+        this.noCompra = noCompra;
+        this.fechaCompra = fechaCompra;
+        this.totalCompra = totalCompra;
+        this.cui = cui;
+    }
+
+    public int getCui() {
+        return cui;
+    }
+
+    public void setCui(int cui) {
+        this.cui = cui;
+    }
+
+    public int getNoCompra() {
+        return noCompra;
+    }
+
+    public void setNoCompra(int noCompra) {
+        this.noCompra = noCompra;
+    }
+
+    public LocalDateTime getFechaCompra() {
+        return fechaCompra;
+    }
+
+    public void setFechaCompra(LocalDateTime fechaCompra) {
+        this.fechaCompra = fechaCompra;
+    }
+
+    public float getTotalCompra() {
+        return totalCompra;
+    }
+
+    public void setTotalCompra(float totalCompra) {
+        this.totalCompra = totalCompra;
+    }
+    
+    
+    
+}
